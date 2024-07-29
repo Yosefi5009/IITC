6c4273c9e04a83a8329f052cbcdac2702f542d03
Yosef hayim sabag <92432561+Yosefi5009@users.noreply.github.com>
2024-07-29 20:57:40 +0300

    Fistuk's Challenge

--- 29.07.2024/Week 2/ABC Challange/main.js
+++ 29.07.2024/Week 2/ABC Challange/main.js
@@ -1,6 +1,8 @@
 // Your Code Goes Here:
 let alphabet = [];
+let j;
 for (let i = 65; i <= 90; i++) {
-    alphabet.push(String.fromCharCode(i));
+    j = String.fromCodePoint(i)
+    alphabet.push(j)
 }
 console.log(alphabet);
