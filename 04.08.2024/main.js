8423e7ad60f72076fcc70ac3643f1af7d70d3871
Yosef hayim sabag <92432561+Yosefi5009@users.noreply.github.com>
2024-08-04 20:20:25 +0300

    omer mazig yt video

---
+++ 04.08.2024/main.js
@@ -0,0 +1,8 @@
+// Youtube video recommended by Omer Mazig:
+// value by reference is the way the computer storage is memory in the values we set to.
+
+// e.g. 
+// if we compare both of c and d we will get false cause the computer storage the value in different way for each.
+
+let c = [1,2] // this equal 0x01
+let d = [1,2] // this equal 0x02
